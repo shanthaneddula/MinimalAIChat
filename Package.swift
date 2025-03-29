e3bbabb6592e4f132b6adbf57686e93faca78d3b
shanthan eddula <shanthaneddula@icloud.com>
2025-03-29 10:29:29 -0700

    Update swift-log dependency version to 0.5.3

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "0.5.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "0.5.3"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
     ],
